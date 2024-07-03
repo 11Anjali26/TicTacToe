e84e84719d10a14759c18201da78d028a6eaebea
Anjali <134794598+11Anjali26@users.noreply.github.com>
2024-07-03 15:48:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".turn-box")[0].classList.add("turn-box-active");
+});
+
+let boxes = document.querySelectorAll(".box");
+
+let turn = "X";
+let isGameOver = false;
+
+boxes.forEach(e =>{
+    e.innerHTML = "";
+    e.addEventListener("click", ()=>{
+        if(!isGameOver && e.innerHTML === ""){
+            e.innerHTML = turn;
+            cheakWin();
+            cheakDraw();
+            changeTurn();
+        }
+    })
+})
+
+function changeTurn(){
+    let turnBoxes = document.querySelectorAll(".turn-box");
+
+    if(turn === "X"){
+        turn = "O";
+        document.querySelector(".bg").style.left = "85px";
+        turnBoxes[0].classList.remove("turn-box-active");
+        turnBoxes[1].classList.add("turn-box-active");
+    } else {
+        turn = "X";
+        document.querySelector(".bg").style.left = "0";
+        turnBoxes[1].classList.remove("turn-box-active");
+        turnBoxes[0].classList.add("turn-box-active");
+    }
+}
+
+function cheakWin(){
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for(let i = 0; i<winConditions.length; i++){
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if(v0 != "" && v0 === v1 && v0 === v2){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = turn + " win";
+            document.querySelector("#play-again").style.display = "inline"
+
+            for(j = 0; j<3; j++){
+                boxes[winConditions[i][j]].style.backgroundColor = "#08D9D6"
+                boxes[winConditions[i][j]].style.color = "#000"
+            }
+        }
+    }
+}
+
+function cheakDraw(){
+    if(!isGameOver){
+        let isDraw = true;
+        boxes.forEach(e =>{
+            if(e.innerHTML === "") isDraw = false;
+        })
+
+        if(isDraw){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = "Draw";
+            document.querySelector("#play-again").style.display = "inline"
+        }
+    }
+}
+
+document.querySelector("#play-again").addEventListener("click", ()=>{
+    isGameOver = false;
+    turn = "X";
+    document.querySelector(".bg").style.left = "0";
+    document.querySelector("#results").innerHTML = "";
+    document.querySelector("#play-again").style.display = "none";
+
+    boxes.forEach(e =>{
+        e.innerHTML = "";
+        e.style.removeProperty("background-color");
+        e.style.color = "#fff"
+    })
+
+    document.querySelectorAll(".turn-box")[0].classList.add("turn-box-active");
+    document.querySelectorAll(".turn-box")[1].classList.remove("turn-box-active");
+})
